68de5ff09980a606d36a5f8ae267963c42514e8b
Nathan Arthur <narthur-git@rainskit.com>
2024-12-21 14:20:44 -0500

    Day 16 part 2

--- 16/maze.py
+++ 16/maze.py
@@ -6,6 +6,7 @@ Advent of Code 2024-16
 
 import argparse
 import heapq
+from collections import defaultdict
 
 OFFSETS = [(-1, 0), (0, 1), (1, 0), (0, -1)]
 DIRECTIONS = ["N", "E", "S", "W"]
@@ -21,21 +22,32 @@ def neighbors(grid, position):
             yield (nr, nc, DIRECTIONS[i])
 
 # A* search courtesy of ChatGPT
-def find_cheapest_path(grid, start_position, start_direction):
+def find_all_paths(grid, start_position, start_direction):
     heap = [(0, start_position, start_direction, [start_position])]
-    visited = set()
+    visited = defaultdict(lambda: float('inf'))
+    min_cost = float('inf')
 
     while heap:
         cost, position, direction, path = heapq.heappop(heap)
 
-        if position in visited:
+        if cost > min_cost:
             continue
-        visited.add(position)
 
         val = grid[position[0]][position[1]]
         debug(position, f"Evaluating position {position} at {cost} with value {val}")
         if val == "E":
-            return cost, path
+            debug(position, "found the end")
+            if cost < min_cost:
+                min_cost = cost
+                yield (cost, path)
+            elif cost == min_cost:
+                yield (cost, path)
+            else:
+                continue
+
+        if visited[(position, direction)] < cost:
+            continue
+        visited[(position, direction)] = cost
 
         for neighbor in neighbors(grid, position):
             new_position, new_direction = neighbor[:2], neighbor[2]
@@ -50,7 +62,17 @@ def find_cheapest_path(grid, start_position, start_direction):
                 (new_cost, new_position, new_direction, path + [new_position])
             )
 
-    raise ValueError("Could not find a path!")
+def find_all_cheapest_paths(grid, start_position, start_direction):
+    paths = defaultdict(lambda: [])
+    for cost, path in find_all_paths(grid, start_position, start_direction):
+        paths[cost].append(path)
+
+    all_nodes = set()
+    min_cost = min(list(paths))
+    print(f"min cost is {min_cost} with {len(paths[min_cost])} paths")
+    for path in paths[min_cost]:
+        all_nodes.update(path)
+    return len(all_nodes)
 
 def print_path(grid, path):
     for r, row in enumerate(grid):
@@ -65,10 +87,8 @@ def main(inputfile):
     with open(inputfile, 'r', encoding='utf-8') as file:
         grid = [list(line.strip()) for line in file]
 
-    start_node = (len(grid) - 2, 1)
-    cost, path = find_cheapest_path(grid, start_node, "E")
+    cost = find_all_cheapest_paths(grid, (len(grid) - 2, 1), "E")
     print(cost)
-    # print_path(grid, path)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Advent of Code 2024-16")
