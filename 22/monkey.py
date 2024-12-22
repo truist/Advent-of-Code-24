fddfdcd978fd940bb6763ee4a0a345f8443ed4bd
Nathan Arthur <narthur-git@rainskit.com>
2024-12-22 14:02:18 -0500

    Simplified the code

--- 22/monkey.py
+++ 22/monkey.py
@@ -8,55 +8,42 @@ import argparse
 
 SEQ_LEN = 4
 
-all_sequences = {}
-def update_all_sequences(sequence, price):
-    if sequence not in all_sequences:
-        all_sequences[sequence] = price
-    else:
-        all_sequences[sequence] += price
-
-class Buyer:
-    def __init__(self):
-        self.secrets = []
-        self.prices = []
-        self.deltas = []
-
-        self.sequence_cache = {}
-
-    def append(self, secret, last_price):
-        self.secrets.append(secret)
-
-        price = secret % 10
-        self.prices.append(price)
-
-        if last_price is not None:
-            self.deltas.append(price - last_price)
-        else:
-            self.deltas.append(None)
-
-        if len(self.deltas) > SEQ_LEN:
-            sequence = tuple(self.deltas[-SEQ_LEN:])
-            if sequence not in self.sequence_cache:
-                self.sequence_cache[sequence] = price
-                update_all_sequences(sequence, price)
-
-        return price
-
 def prune_mix(secret, modified):
     return (modified ^ secret) % 16777216
 
-def iterate(secret, count):
-    buyer = Buyer()
+def append(secret, last_price, deltas, only_first_sequences, all_sequences):
+    price = secret % 10
 
-    last_price = buyer.append(secret, None)
+    if last_price is not None:
+        deltas.append(price - last_price)
+    else:
+        deltas.append(None)
+
+    if len(deltas) > SEQ_LEN:
+        sequence = tuple(deltas[-SEQ_LEN:])
+        if sequence not in only_first_sequences:
+            only_first_sequences.add(sequence)
+
+            if sequence not in all_sequences:
+                all_sequences[sequence] = price
+            else:
+                all_sequences[sequence] += price
+
+    return price
+
+def iterate(secret, count, all_sequences):
+    deltas = []
+    only_first_sequences = set()
+
+    last_price = append(secret, None, deltas, only_first_sequences, all_sequences)
     for _ in range(count):
         secret = prune_mix(secret * 64, secret)
         secret = prune_mix(secret // 32, secret)
         secret = prune_mix(secret * 2048, secret)
 
-        last_price = buyer.append(secret, last_price)
+        last_price = append(secret, last_price, deltas, only_first_sequences, all_sequences)
 
-def find_optimal_sequence():
+def find_optimal_sequence(all_sequences):
     max_price = 0
     best_sequence = None
     for sequence, price in all_sequences.items():
@@ -71,10 +58,11 @@ def main(inputfile):
     with open(inputfile, 'r', encoding='utf-8') as file:
         buyer_seeds = [int(line.strip()) for line in file]
 
+    all_sequences = {}
     for seed in buyer_seeds:
-        iterate(seed, 2000)
+        iterate(seed, 2000, all_sequences)
 
-    sequence, total = find_optimal_sequence()
+    sequence, total = find_optimal_sequence(all_sequences)
     print(sequence)
     print(total)
 
