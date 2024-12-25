4c431276024383fe1b06d63c8be638e5549cd379
Nathan Arthur <narthur-git@rainskit.com>
2024-12-24 23:53:54 -0500

    Much faster searching but still impossibly slow

--- 24/gates.py
+++ 24/gates.py
@@ -16,24 +16,79 @@ class Gate:
     out: str
     value: int
 
+    def calculate(self, left, right):
+        if self.op == "AND":
+            return left & right
+        if self.op == "OR":
+            return left | right
+        if self.op == "XOR":
+            return left ^ right
+        raise ValueError(f"Unknown op: {self.op}")
+
+    def swap_outputs(self, other_gate):
+        tmp_val = other_gate.value
+        other_gate.value = self.value
+        self.value = tmp_val
+
+
 # basically just a holder for globals
 @dataclass
 class Circuit:
-    gates: list
+    gates: dict
     inputs: list
     outputs: list
 
-def generate_inputs(input_lines):
+    def reset_signals(self):
+        for gate in self.gates.values():
+            gate.value = None
+
+    def find_swap_partner(self, first_gate, swaps):
+        index = swaps.index(first_gate.out)
+        if index % 2 == 0:
+            other = swaps[index + 1]
+        else:
+            other = swaps[index - 1]
+        return self.gates[other]
+
+    def find_val(self, name, swaps):
+        if name in self.inputs:
+            return self.inputs[name]
+
+        return self.calc_output(self.gates[name], swaps, True)
+
+    def calc_output(self, gate, swaps, do_swap):
+        if gate.value is None:
+            val1 = self.find_val(gate.in1, swaps)
+            val2 = self.find_val(gate.in2, swaps)
+            gate.value = gate.calculate(val1, val2)
+
+            if do_swap and gate.out in swaps:
+                other_gate = self.find_swap_partner(gate, swaps)
+                self.calc_output(other_gate, swaps, False)
+                gate.swap_outputs(other_gate)
+
+        return gate.value
+
+    def propagate_to_zero(self, swaps):
+        self.reset_signals()
+        for index in range(len(self.outputs)): # pylint: disable=consider-using-enumerate
+            result = self.calc_output(self.gates[f"z{index:02}"], swaps, True)
+            self.outputs[index] = result
+            if result == 0:
+                return index
+        return len(self.outputs) + 1
+
+def generate_inputs(input_lines, x_val, y_val):
     inputs = {}
     max_x = 0
     for input_line in input_lines.split("\n"):
         name, _ = input_line.split(":")
         name = name.strip()
         if name.startswith("x"):
-            inputs[name] = 0
+            inputs[name] = x_val
             max_x = max(max_x, int(name[1:3]))
         else:
-            inputs[name] = 1
+            inputs[name] = y_val
 
     outputs = []
     for _ in range(max_x + 2):
@@ -53,107 +108,72 @@ def parse_gates(gate_lines):
         gates[gate.out] = gate
     return gates
 
-def do_gate(op, left, right):
-    if op == "AND":
-        return left & right
-    if op == "OR":
-        return left | right
-    if op == "XOR":
-        return left ^ right
-    raise ValueError(f"Unknown op: {op}")
-
-def swap_outputs(gate, other_gate):
-    tmp_val = other_gate.value
-    other_gate.value = gate.value
-    gate.value = tmp_val
-
-def find_swap_partner(gates, swaps, first_gate):
-    index = swaps.index(first_gate.out)
-    if index % 2 == 0:
-        other = swaps[index + 1]
-    else:
-        other = swaps[index - 1]
-    return gates[other]
-
-def find_val(name, circuit, swaps):
-    if name in circuit.inputs:
-        return circuit.inputs[name]
-
-    return calc_output(circuit.gates[name], circuit, swaps, True)
-
-def calc_output(gate, circuit, swaps, do_swap):
-    if gate.value is None:
-        val1 = find_val(gate.in1, circuit, swaps)
-        val2 = find_val(gate.in2, circuit, swaps)
-        gate.value = do_gate(gate.op, val1, val2)
-
-        if do_swap and gate.out in swaps:
-            other_gate = find_swap_partner(circuit.gates, swaps, gate)
-            calc_output(other_gate, circuit, swaps, False)
-            swap_outputs(gate, other_gate)
-
-    return gate.value
-
-def propagate_signals(circuit, swaps):
-    reset_signals(circuit.gates)
-    for name, gate in circuit.gates.items():
-        if name.startswith("z"):
-            circuit.outputs[int(name[1:3])] = calc_output(gate, circuit, swaps, True)
-
-def reset_signals(gates):
-    for gate in gates.values():
-        gate.value = None
-
-def find_upstreams(name, gates):
-    if name not in gates:
-        return []
-    gate = gates[name]
-    return [name] + find_upstreams(gate.in1, gates) + find_upstreams(gate.in2, gates)
+def make_circuit(input_lines, gate_lines, x_val, y_val):
+    inputs, outputs = generate_inputs(input_lines, x_val, y_val)
+    gates = parse_gates(gate_lines)
+    return Circuit(gates, inputs, outputs)
 
 def bits_to_decimal(bits):
     return int("".join(map(str, bits)), 2)
 
-def find_swaps(index, circuit, swaps):
+def propagate_to_zero(circuit1, circuit2, swaps):
+    return min(circuit1.propagate_to_zero(swaps), circuit2.propagate_to_zero(swaps))
+
+def find_swaps(index, circuit1, circuit2, swaps):
     print(f"looking for swap to fix bit {index}")
-    # print(f"known swaps: {swaps}")
-    for new_swaps in combinations(circuit.gates.keys(), 2):
+    print(f"known swaps: {swaps}")
+    for new_swaps in combinations(circuit1.gates.keys(), 2):
         if new_swaps[0] in swaps or new_swaps[1] in swaps:
             continue
-        all_swaps = list(new_swaps) + swaps
-        propagate_signals(circuit, all_swaps)
-        # print(bits_to_decimal(outputs))
-        if 0 not in circuit.outputs[0:index + 1]:
-            # print(f"found swap! {new_swaps}")
-            # print(f"fixed bit {index}: {outputs}")
-            try:
-                next_zero = circuit.outputs.index(0, index + 1)
-            except ValueError:
-                print("no more zeros!")
-                return all_swaps
 
-            downstream_swaps = find_swaps(next_zero, circuit, all_swaps)
+        combined_swaps = list(new_swaps) + swaps
+        next_zero = propagate_to_zero(circuit1, circuit2, combined_swaps)
+        if next_zero > index:
+            # print(f"found swap! {new_swaps}")
+            # print("circuit 1 outputs:", circuit1.outputs, bits_to_decimal(circuit1.outputs))
+            # print("circuit 2 outputs:", circuit2.outputs, bits_to_decimal(circuit2.outputs))
+
+            downstream_swaps = handle_zeros(next_zero, circuit1, circuit2, combined_swaps)
             if downstream_swaps is not None:
                 print("success at the end!")
                 return downstream_swaps
 
-    print("reached the end and couldn't find a swap")
+    print(f"can't find suitable swaps for {index}")
     return None
+
+def handle_zeros(next_zero, circuit1, circuit2, swaps):
+    if next_zero > len(circuit1.outputs):
+        print("no more zeros!")
+        return swaps
+
+    if len(swaps) == 8:
+        # print("hit swap limit")
+        return None
+
+    downstream_swaps = find_swaps(next_zero, circuit1, circuit2, swaps)
+    if downstream_swaps is not None:
+        print("success at the end!")
+        return downstream_swaps
+
+    print(f"couldn't handle the zero at {next_zero}")
+    return None
+
 
 def main(inputfile):
     with open(inputfile, 'r', encoding='utf-8') as file:
         input_lines, gate_lines = file.read().split("\n\n")
 
-    inputs, outputs = generate_inputs(input_lines)
-    gates = parse_gates(gate_lines)
-    circuit = Circuit(gates, inputs, outputs)
+    circuit1 = make_circuit(input_lines, gate_lines, 1, 0)
+    circuit2 = make_circuit(input_lines, gate_lines, 0, 1)
 
     swaps = []
-    propagate_signals(circuit, swaps)
-    print(circuit.outputs)
+    next_zero = propagate_to_zero(circuit1, circuit2, swaps)
+    print(circuit1.outputs)
+    print(circuit2.outputs)
 
-    swaps = find_swaps(circuit.outputs.index(0), circuit, swaps)
-    print(circuit.outputs)
-
+    swaps = handle_zeros(next_zero, circuit1, circuit2, swaps)
+    print(circuit1.outputs)
+    print(circuit2.outputs)
     print(swaps)
 
 if __name__ == "__main__":
